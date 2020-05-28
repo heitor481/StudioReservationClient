ad2dab62c44795143a8c67da4e1f4579d217494c
heitor481 <heitor_481@hotmail.com>
2020-05-27 22:18:51 -0300

    Creating the login component

---
+++ studio-reservation-client/src/app/App.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Login from '../features/login/login';
+
+function App() {
+  return (
+    <Fragment>
+        <Login />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ studio-reservation-client/src/features/login/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () =>{
+    return (
+        <div>
+            <h1>This is working</h1>
+        </div>
+    );
+}
+
+export default Login
